b58de2bf173a00ffb7c952e962a2301a7f48d3bb
eugene <eugeneholenstein@gmail.com>
2023-07-19 14:20:51 +1000

    commit tree a18fc0386834eb13a6e2ce3841bb32083c3558c3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    belongs_to :author
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction' 'Non-Fiction'] }
+    validate :not_click_baity
+
+    def not_click_baity
+        self.title != "True Facts"
+    end
 end
